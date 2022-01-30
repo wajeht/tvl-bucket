0809afb70795b9b75b12433fc4c40fe8a313bb40
wajeht <58354193+wajeht@users.noreply.github.com>
2022-01-30 17:45:35 -0500

    feat: add username column to video schema

--- database/migrations/20220128050552_video.js
+++ database/migrations/20220128050552_video.js
@@ -3,6 +3,7 @@ exports.up = (knex) => {
 		table.increments('id').primary().notNullable();
 		table.string('screenshot_path').notNullable();
 		table.string('video_path').notNullable();
+		table.string('username').notNullable();
 		table.integer('user_id').notNullable();
 		table
 			.integer('video_details_id')
