9b22edfd6aa5dcd7eac8a934e6dbe3807146f7cd
wajeht <58354193+wajeht@users.noreply.github.com>
2022-01-31 23:27:06 -0500

    feat: check for to see if folder has been make

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -1,7 +1,10 @@
 const uuid = require('uuid').v4;
 const multer = require('multer');
 
+const fs = require('fs');
 const path = require('path');
+
+const { root } = require('../../utils/directory.js');
 
 const fileFilter = (req, file, cb) => {
 	const fileTypes = /jpeg|jpg|png|gif|mp4|mov|mpeg/;
@@ -17,12 +20,25 @@ const fileFilter = (req, file, cb) => {
 
 const fileStorage = multer.diskStorage({
 	destination: (req, file, cb) => {
-		// const { username } = req.session;
-		const username = 'jaw';
-		cb(null, `public/upload/${username}/`);
+		const { username } = req.body;
+		const userFolder = path.join(root, `src/public/upload/${username}`);
+
+		// if !folder, make one
+		// TODO: fix a bug where it breaks on no upload folder
+		if (!fs.existsSync(userFolder)) {
+			fs.mkdir(userFolder, { recursive: true }, (err) => {
+				if (!err) {
+					throw new Error(
+						`Something went wrong while making a folder for ${username}'s upload!`,
+					);
+				}
+			});
+		}
+
+		cb(null, userFolder);
 	},
 	filename: (req, file, cb) => {
-		cb(null, uuid());
+		cb(null, uuid() + '.' + file.originalname.split('.')[1]);
 	},
 });
 
