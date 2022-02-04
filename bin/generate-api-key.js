f983d0065bf2f244eff063e4e505a040cdda8c19
wajeht <58354193+wajeht@users.noreply.github.com>
2022-02-03 21:22:07 -0500

    refactor: code format

--- bin/generate-api-key.js
+++ bin/generate-api-key.js
@@ -11,14 +11,15 @@ const db = require('../database/db.js');
 		const hashedApiKey = await bcrypt.hash(APIKey, 10);
 		if (!hashedApiKey) throw new Error('Something went wrong while hashing your API key!');
 
-		const storeApiKey = await db.insert({ key: APIKey, hashed_key: hashedApiKey })
-																.into('api_key')
-																.returning('*');
+		const storeApiKey = await db
+			.insert({ key: APIKey, hashed_key: hashedApiKey })
+			.into('api_key')
+			.returning('*');
+			
 		if (!storeApiKey.length)throw new Error('Something went wrong while storing your API key!');
 
 		console.log({ 'X-API-KEY': APIKey });
 		process.exit(1);
-
 	} catch (err) {
 		console.log(err);
 		process.exit(1);
