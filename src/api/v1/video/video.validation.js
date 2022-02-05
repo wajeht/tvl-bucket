cac5438e6e35e42c0aa794c1fff633920c35ca5e
wajeht <58354193+wajeht@users.noreply.github.com>
2022-02-04 23:17:06 -0500

    feat: add video upload validation

--- src/api/v1/video/video.validation.js
+++ src/api/v1/video/video.validation.js
@@ -1,7 +1,33 @@
-const param  = require('express-validator').param;
+const { param, body } = require('express-validator');
 
-const VGetVideo = [param('id').isInt().toInt()];
+class VideoValidation {
+	getVideo = [param('id', 'The format is not correct!').isInt().toInt()];
+	postVideo = [
+		body('username')
+			.trim()
+			.isEmpty()
+			.withMessage('The value must not be empty!'),
+		body('user_id').isEmpty().withMessage('The value must not be empty!'),
+		// .isInt()
+		// .withMessage('The value must be an integer!'),
+		body('video').isEmpty().withMessage('The value must not be empty!'),
+	];
+	deleteVideo = [
+		param('id', 'Oops! The format is not correct').isInt().toInt(),
+	];
+	updateVideo = [
+		body('username', 'Oops! The format is not correct')
+			.not()
+			.isEmpty()
+			.trim()
+			.escape(),
+		body('user_id', 'Oops! The format is not correct')
+			.not()
+			.isEmpty()
+			.trim()
+			.escape(),
+		body('video', 'Oops! The format is not correct').isInt().toInt(),
+	];
+}
 
-module.exports = {
-	VGetVideo,
-};
+module.exports = new VideoValidation();
