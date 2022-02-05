32341b44c3e2ae4eae254ba79bc5502cccbafc3c
wajeht <58354193+wajeht@users.noreply.github.com>
2022-02-05 04:29:00 -0500

    refactor: user on params instead of multiple

--- src/api/v1/video/video.router.js
+++ src/api/v1/video/video.router.js
@@ -4,22 +4,17 @@ const router = express.Router();
 const { uploadVideo } = require('../../../middlewares/upload.js');
 const { validate } = require('../../../middlewares/validator.js');
 const VideoValidation = require('./video.validation.js');
+const VideoController = require('./video.controller.js');
 
-const {
-	getVideo,
-	getVideos,
-	postVideo,
-	updateVideo,
-	deleteVideo,
-} = require('./video.controller.js');
+router
+	.route('/')
+	.get(VideoController.getVideos)
+	.post(uploadVideo, validate(VideoValidation.postVideo), VideoController.postVideo);
 
-router.route('/')
-			.get(getVideos)
-			.post(validate(VideoValidation.postVideo), uploadVideo, postVideo);
-
-router.route('/:id')
-			.get(validate(VideoValidation.getVideo), getVideo)
-			.patch(validate(VideoValidation.updateVideo), uploadVideo, updateVideo)
-			.delete(validate(VideoValidation.deleteVideo), deleteVideo);
+router
+	.route('/:id')
+	.get(validate(VideoValidation.getVideo), VideoController.getVideo)
+	.patch(validate(VideoValidation.updateVideo), uploadVideo, VideoController.updateVideo)
+	.delete(validate(VideoValidation.deleteVideo), VideoController.deleteVideo);
 
 module.exports = router;
