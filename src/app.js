147bf09685a9359d72869ec029cce5ee3772e87c
wajeht <58354193+wajeht@users.noreply.github.com>
2022-02-05 13:45:26 -0500

    feat: enable cors to access from outside resources

--- src/app.js
+++ src/app.js
@@ -8,10 +8,12 @@ const v1 = require('./api/v1/index.js');
 
 const compression = require('compression');
 const helmet = require('helmet');
+const cors = require('cors');
 const { errorHandler, notFoundHandler } = require('./middlewares/error.js');
 
 app.use(helmet());
 app.use(compression());
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
