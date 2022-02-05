d27165990842883fc85bffd1501f59da52a1adbb
wajeht <58354193+wajeht@users.noreply.github.com>
2022-02-04 23:16:19 -0500

    feat: update response messages for auth

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,18 +1,27 @@
 const bcrypt = require('bcryptjs');
 const db = require('../../database/db.js');
+const { StatusCodes } = require('http-status-codes');
 
 const auth = async (req, res, next) => {
 	try {
 		const api = req.get('X-API-KEY');
 
 		if (!api || api == undefined) {
-			throw new Error('No api key!');
+			return res.status(StatusCodes.UNAUTHORIZED).json({
+				status: 'fail',
+				request_url: req.originalUrl,
+				message: 'Authentication credentials were missing!',
+			});
 		}
 
 		const isKey = await db.select('*').from('api_key').where({ key: api });
 
 		if (!isKey.length) {
-			throw new Error('Invalid api key!');
+			return res.status(StatusCodes.UNAUTHORIZED).json({
+				status: 'fail',
+				request_url: req.originalUrl,
+				message: 'Authentication credentials were invalid!',
+			});
 		}
 
 		const { hashed_key } = isKey[0];
@@ -20,7 +29,11 @@ const auth = async (req, res, next) => {
 		const sameKey = await bcrypt.compare(api, hashed_key);
 
 		if (!sameKey) {
-			throw new Error('Wrong api key!');
+			return res.status(StatusCodes.UNAUTHORIZED).json({
+				status: 'fail',
+				request_url: req.originalUrl,
+				message: 'Authentication credentials were incorrect!',
+			});
 		}
 
 		next();
