4e143103f9abe67344844c85ba951fd34e211a12
wajeht <58354193+wajeht@users.noreply.github.com>
2022-02-05 13:45:50 -0500

    feat: redefined schema

--- database/migrations/20220130175337_video_details.js
+++ database/migrations/20220130175337_video_details.js
@@ -2,8 +2,12 @@ exports.up = (knex) => {
 	return knex.schema.createTable('video_details', (table) => {
 		table.increments('id').primary().notNullable();
 		table.string('filename').notNullable();
-		table.string('size').notNullable();
+		table.integer('size').notNullable();
 		table.string('mimetype').notNullable();
+		table.string('absolute_video_path').notNullable();
+		table
+			.string('absolute_screenshot_path')
+			.defaultTo('/default/video_place_holder.jpeg');
 		table.timestamps(true, true);
 	});
 };
