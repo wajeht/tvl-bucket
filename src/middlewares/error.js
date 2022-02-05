1772a728ef1625d0a9f6f731f47f974e893b2f76
wajeht <58354193+wajeht@users.noreply.github.com>
2022-02-04 23:39:49 -0500

    feat: add err stack message for local env

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -1,4 +1,5 @@
 const StatusCodes = require('http-status-codes').StatusCodes;
+const { env } = require('../../config/env.js');
 
 const notFoundHandler = (req, res, next) => {
 	res.status(StatusCodes.NOT_FOUND).json({
@@ -9,9 +10,14 @@ const notFoundHandler = (req, res, next) => {
 };
 
 const errorHandler = (err, req, res, next) => {
-	res
-		.status(StatusCodes.INTERNAL_SERVER_ERROR)
-		.json({ request_url: req.originalUrl, message: err.message });
+	res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+		status: 'fail',
+		request_url: req.originalUrl,
+		message:
+			env == 'development'
+				? err.stack
+				: 'The server encountered an internal error or misconfiguration and was unable to complete your request.',
+	});
 };
 
 module.exports = {
