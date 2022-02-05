11c5f7af0323c2a62c72fd9201f256b12b8aa60e
wajeht <58354193+wajeht@users.noreply.github.com>
2022-02-05 04:28:39 -0500

    feaet: add validation schema for video controller

--- src/api/v1/video/video.validation.js
+++ src/api/v1/video/video.validation.js
@@ -1,32 +1,65 @@
-const { param, body } = require('express-validator');
+const { check, param, body } = require('express-validator');
 
 class VideoValidation {
-	getVideo = [param('id', 'The format is not correct!').isInt().toInt()];
+	/**
+	 * getVideo Controller Validation
+	 */
+	getVideo = [
+		param('id').trim().isInt().withMessage('The value must be an integer!'),
+	];
+
+	/**
+	 * postVideo Controller Validation
+	 */
 	postVideo = [
 		body('username')
 			.trim()
-			.isEmpty()
+			.notEmpty()
 			.withMessage('The value must not be empty!'),
-		body('user_id').isEmpty().withMessage('The value must not be empty!'),
-		// .isInt()
-		// .withMessage('The value must be an integer!'),
-		body('video').isEmpty().withMessage('The value must not be empty!'),
+		body('user_id')
+			.trim()
+			.notEmpty()
+			.withMessage('The value must not be empty!')
+			.isInt()
+			.withMessage('The value must be an integer!'),
+		check('video')
+			.custom((value, { req }) => {
+				if (req.file) {
+					return true;
+				}
+				return false;
+			})
+			.withMessage('File must be provided!'),
 	];
+
+	/**
+	 * deleteVideo Controller Validation
+	 */
 	deleteVideo = [
-		param('id', 'Oops! The format is not correct').isInt().toInt(),
+		param('id')
+			.trim()
+			.notEmpty()
+			.withMessage('The value must not be empty!')
+			.isInt()
+			.withMessage('The value must be an integer!'),
 	];
+
+	/**
+	 * TODO:// test this schema
+	 * updateVideo Controller Validation
+	 */
 	updateVideo = [
-		body('username', 'Oops! The format is not correct')
+		body('username', 'The format is not correct!')
 			.not()
 			.isEmpty()
 			.trim()
 			.escape(),
-		body('user_id', 'Oops! The format is not correct')
+		body('user_id', 'The format is not correct!')
 			.not()
 			.isEmpty()
 			.trim()
 			.escape(),
-		body('video', 'Oops! The format is not correct').isInt().toInt(),
+		body('video', 'The format is not correct!').isInt().toInt(),
 	];
 }
 
