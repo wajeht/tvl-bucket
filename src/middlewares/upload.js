6e4b6a5ef607f5cad237364e045f485945b15a30
wajeht <58354193+wajeht@users.noreply.github.com>
2022-02-06 15:45:02 -0500

    refactor: separate logic into a util file

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -4,7 +4,7 @@ const multer = require('multer');
 const fs = require('fs');
 const path = require('path');
 
-const { root } = require('../../utils/directory.js');
+const makeUserDataFolder = require('../../utils/make-user-data-folder.js');
 
 const fileFilter = (req, file, cb) => {
 	const fileTypes = /jpeg|jpg|png|gif|mp4|mov|mpeg/;
@@ -21,11 +21,10 @@ const fileFilter = (req, file, cb) => {
 const fileStorage = multer.diskStorage({
 	destination: (req, file, cb) => {
 		const { username } = req.body;
-		const userFolder = path.join(root, `src/public/upload/${username}`);
+		const folder = makeUserDataFolder(username);
 		setTimeout(() => {
-			cb(null, userFolder);
+			cb(null, folder);
 		}, 5);
-		fs.mkdir(userFolder, { recursive: true }, (err) => true);
 	},
 	filename: (req, file, cb) => {
 		cb(null, uuid() + '.' + file.originalname.split('.')[1]);
