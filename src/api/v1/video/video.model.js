b35139b588a84f612417dc8f4cdb7cdabaf8a0bf
wajeht <58354193+wajeht@users.noreply.github.com>
2022-02-08 19:12:03 -0500

    feat: add oop

--- src/api/v1/video/video.model.js
+++ src/api/v1/video/video.model.js
@@ -1,54 +1,58 @@
-const db = require('../../../../database/db.js');
+const db = require("../../../../database/db.js");
 
 class VideoModel {
-	getVideo = (id) => {
-		return db.select('*').from('video').where({ id });
-	};
+  #db;
 
-	getVideos = () => {
-		return db.select('*').from('video');
-	};
+  constructor() {
+    this.#db = db;
+  }
 
-	postVideo = (body, video) => {
-		return db
-			.insert({
-				username: body.username,
-				user_id: body.user_id,
-				video_path: video.path.slice(video.path.indexOf('/upload')),
-			})
-			.into('video')
-			.returning('*')
-			.then(async (res) => {
-				const { id } = res[0];
-				await db
-					.insert({
-						filename: video.filename,
-						size: video.size,
-						mimetype: video.mimetype,
-						video_id: id,
-						absolute_video_path: video.path,
-					})
-					.into('video_details');
-				return res;
-			});
-	};
+  getVideo(id) {
+    return this.#db.select("*").from("video").where({ id });
+  }
 
-	updateVideoScreenshot = (postBody) => {};
+  getVideos() {
+    return this.#db.select("*").from("video");
+  }
 
-	updateVideo = (id, video) => {
-		return db
-			.update({
-				screenshot_path: video.screenshot_path,
-				video_path: video.video_path,
-			})
-			.from('video')
-			.where({ id })
-			.returning('*');
-	};
+  postVideo(body, video) {
+    return this.#db
+      .insert({
+        username: body.username,
+        user_id: body.user_id,
+        video_path: video.path.slice(video.path.indexOf("/upload")),
+      })
+      .into("video")
+      .returning("*")
+      .then(async (res) => {
+        const { id } = res[0];
+        await db
+          .insert({
+            filename: video.filename,
+            size: video.size,
+            mimetype: video.mimetype,
+            video_id: id,
+            absolute_video_path: video.path,
+          })
+          .into("video_details");
+        return res;
+      });
+  }
 
-	deleteVideo = (id) => {
-		return db.del().from('video').where({ id }).returning('*');
-	};
+  updateVideo(id, video) {
+    return this.#db
+      .update({
+        screenshot_path: video.screenshot_path,
+        video_path: video.video_path,
+      })
+      .from("video")
+      .where({ id })
+      .returning("*");
+  }
+
+  deleteVideo(id) {
+    return this.#db.del().from("video").where({ id }).returning("*");
+  }
 }
 
 module.exports = new VideoModel();
